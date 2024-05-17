e051ad94d635f36acca65810bd68c75884f9f076
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2024-05-18 06:08:51 +0800

    :tada: property detail page with components
    - show the property detail
    - Loading Spinner
    - Property Detail Component
    - Backend for showing list of properties, property detail

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
-import properties from '@/properties.json';
 import PropertyCard from '@/components/PropertyCard';
+import { fetchProperties } from '@/app/actions';
 
-function PropertiesPage() {
-  console.log(properties);
+async function PropertiesPage() {
+  const properties = await fetchProperties();
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => {
-            return <PropertyCard property={property} key={property._id} />;
-          })}
+          {properties.length === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            properties.map((property) => {
+              return <PropertyCard property={property} key={property._id} />;
+            })
+          )}
         </div>
       </div>
     </section>
